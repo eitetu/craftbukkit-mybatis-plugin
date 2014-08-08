b1f6502f331d53a03fd075c52e1599d19002275f
eitetu <eitetu87@gmail.com>
2014-08-08 11:47:32 +0900

    project initilaize.

---
+++ mybatis/src/main/java/com/eitetu/craftbukkit/mybatis/MybatisPlugin.java
@@ -0,0 +1,17 @@
+package com.eitetu.craftbukkit.mybatis;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class MybatisPlugin extends JavaPlugin {
+	@Override
+	public void onEnable() {
+		super.onEnable();
+
+
+	}
+
+	@Override
+	public void onDisable() {
+		super.onDisable();
+	}
+}
